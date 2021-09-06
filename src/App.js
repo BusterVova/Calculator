253300d32f5d41cee486b3d09fcc269b4f9f3fc6
BusterVova <vova_13_frolov@mail.ru>
2021-09-06 15:06:57 +0400

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint no-eval :0*/
+import "./App.css";
+import React from "react";
+import operators from "./operators";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      out: "0",
+    };
+    this.refOutput = React.createRef();
+  }
+  clickedNumber(value) {
+    let currentValue = value;
+    let output = this.refOutput.current;
+    this.setState({
+      out: currentValue,
+    });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    if (output.value === "0") {
+      output.value = "";
+    }
+    output.value += currentValue;
+  }
+
+  clickedOperation(value) {
+    let output = this.refOutput.current;
+    if (value === "AC") {
+      output.value = "0";
+    } else if (value === "=") {
+      try {
+        output.value = eval(output.value);
+      } catch {
+        alert("Недопустимое значение");
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <input
+          className="input"
+          ref={this.refOutput}
+          type="text"
+          defaultValue={this.state.out}
+        />
+        <div className="buttons">
+          <div className="operators">
+            {operators.buttons.map((el) => (
+              <button
+                key={el.value}
+                onClick={() => this.clickedNumber(el.value)}
+              >
+                {el.value}
+              </button>
+            ))}
+          </div>
+          <div className="leftPanel">
+            <div className="numbers">
+              {operators.numbersOne.map((el) => (
+                <button
+                  key={el.value}
+                  onClick={() => this.clickedNumber(el.value)}
+                >
+                  {el.value}
+                </button>
+              ))}
+            </div>
+            <div className="numbers">
+              {operators.numbersTwo.map((el) => (
+                <button
+                  key={el.value}
+                  onClick={() => this.clickedNumber(el.value)}
+                >
+                  {el.value}
+                </button>
+              ))}
+            </div>
+            <div className="numbers">
+              {operators.numbersThree.map((el) => (
+                <button
+                  key={el.value}
+                  onClick={() => this.clickedNumber(el.value)}
+                >
+                  {el.value}
+                </button>
+              ))}
+            </div>
+            <div className="numbers">
+              {operators.operations.map((el) => (
+                <button
+                  key={el.value}
+                  onClick={() => this.clickedNumber(el.value)}
+                >
+                  {el.value}
+                </button>
+              ))}
+              {operators.AC.map((el) => (
+                <button
+                  key={el.value}
+                  onClick={() => this.clickedOperation(el.value)}
+                >
+                  {el.value}
+                </button>
+              ))}
+            </div>
+          </div>
+
+          {operators.default.map((el) => (
+            <button
+              className="equal"
+              key={el.value}
+              onClick={() => this.clickedOperation(el.value)}
+            >
+              {el.value}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/operators.js
@@ -0,0 +1,68 @@
+let operators = {
+  buttons: [
+    {
+      value: "+",
+    },
+    {
+      value: "-",
+    },
+    {
+      value: "*",
+    },
+    {
+      value: "/",
+    },
+  ],
+  operations: [
+    {
+      value: "0",
+    },
+    {
+      value: ".",
+    },
+  ],
+  AC: [
+    {
+      value: "AC",
+    },
+  ],
+  numbersOne: [
+    {
+      value: "7",
+    },
+    {
+      value: "8",
+    },
+    {
+      value: "9",
+    },
+  ],
+  numbersTwo: [
+    {
+      value: "4",
+    },
+    {
+      value: "5",
+    },
+    {
+      value: "6",
+    },
+  ],
+  numbersThree: [
+    {
+      value: "1",
+    },
+    {
+      value: "2",
+    },
+    {
+      value: "3",
+    },
+  ],
+  default: [
+    {
+      value: "=",
+    },
+  ],
+};
+export default operators;
